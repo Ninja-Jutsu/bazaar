50809405075c0651a3efb4bf1478eedfd5779086
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-09-20 02:23:32 +0800

    Create the main Container component

--- components/globals/Container.tsx
+++ components/globals/Container.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { cn } from '@/lib/utils'
 
-export default function Container() {
+function Container({
+  children,
+  className,
+}: {
+  children: React.ReactNode
+  className?: string
+}) {
   return (
-    <div>
-      
+    <div className={cn('mx-auto max-w-6xl xl:max-w-7xl px-8', className)}>
+      {children}
     </div>
   )
 }
+export default Container
