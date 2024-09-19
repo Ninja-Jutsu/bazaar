5ad85932b867364ec9b2f6571310891ce4b2310f
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-09-20 05:57:23 +0800

    Setup Prisma & Prisma Client

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from '@/components/ui/dropdown-menu'
+import { LuAlignLeft } from 'react-icons/lu'
+import Link from 'next/link'
+import { Button } from '../ui/button'
+import { links } from '@/utils/links'
 
-export default function LinksDropdown() {
+function LinksDropdown() {
   return (
-    <div>
-      
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant='outline'
+          className='flex gap-4 max-w-[100px]'
+        >
+          <LuAlignLeft className='w-6 h-6' />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='w-40'
+        align='start'
+        sideOffset={10}
+      >
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link
+                href={link.href}
+                className='capitalize w-full'
+              >
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          )
+        })}
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link
+            href='/sign-in'
+            className='capitalize w-full'
+          >
+            sign in
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
+export default LinksDropdown
