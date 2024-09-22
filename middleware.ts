3c8927c6e0dfaf6b0791416a4d35eb7cef0f606c
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-09-23 04:53:30 +0800

    Build admin route & components

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isPublicRoute = createRouteMatcher(['/', '/products(.*)', '/about'])
-
+const isAdminRoute = createRouteMatcher(['/adlin(.*)'])
 export default clerkMiddleware((auth, req) => {
+  const isAdminUser = auth().userId === process.env.ADMIN_USER_ID
+
+  // if trying to access admin and not admin, then redirect to home page
+  if (isAdminRoute(req) && !isAdminUser) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
   if (!isPublicRoute(req)) auth().protect()
 })
 
