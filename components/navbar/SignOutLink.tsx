251501846994bc132e1788215e02a6da675b0187
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-09-23 04:13:11 +0800

    Create the SignOutButton Using Clerk

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+'use client'
+import { SignOutButton } from '@clerk/nextjs'
+import { useToast } from '../../hooks/use-toast'
+import Link from 'next/link'
 
-export default function SignOutLink() {
+function SignOutLink() {
+  const { toast } = useToast()
+  const handleLogout = () => {
+    toast({ description: 'Logging Out...' })
+  }
   return (
-    <div>
-      
-    </div>
+    <SignOutButton>
+      <Link
+        href='/'
+        className='w-full text-left'
+        onClick={handleLogout}
+      >
+        Logout
+      </Link>
+    </SignOutButton>
   )
 }
+export default SignOutLink
