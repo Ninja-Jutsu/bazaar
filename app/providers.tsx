b58a133bde599dffb33f9d119bc1bb602d89b4fa
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-09-23 03:37:46 +0800

    Setup Clerk Auth + Middleware

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,12 @@
 'use client'
 import { ThemeProvider } from '@/components/theme-provider'
 import ProductProvider from '@/components/contexts/ProductContext'
+import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from '@/components/ui/toaster'
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <ClerkProvider>
       <Toaster />
       <ThemeProvider
         attribute='class'
@@ -15,7 +16,7 @@ function Providers({ children }: { children: React.ReactNode }) {
       >
         <ProductProvider>{children}</ProductProvider>
       </ThemeProvider>
-    </>
+    </ClerkProvider>
   )
 }
 export default Providers
