798a8909e94ae5d4cd407f82c38662466fad0c21
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-09-24 14:22:01 +0800

    Create Reviews components files

--- utils/actions.ts
+++ utils/actions.ts
@@ -248,3 +248,18 @@ export const fetchUserFavorites = async () => {
   })
   return favorites
 }
+
+
+// Reviews:
+export const createReviewAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  return { message: 'review submitted successfully' }
+}
+
+export const fetchProductReviews = async () => {}
+export const fetchProductReviewsByUser = async () => {}
+export const deleteReviewAction = async () => {}
+export const findExistingReview = async () => {}
+export const fetchProductRating = async () => {}
