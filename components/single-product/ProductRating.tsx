49ef9b9dd4996fd48e68e8f0443973d1e82dd0a7
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-09-25 09:31:09 +0800

    Create product Rating and setup its action func

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { fetchProductRating } from '@/utils/actions'
 import { FaStar } from 'react-icons/fa'
 
 async function ProductRating({ productId }: { productId: string }) {
-  const rating = 4.2
-  const count = 25
+  // temp
+  // const rating = 4.2
+  // const count = 25
+  const { rating, count } = await fetchProductRating(productId)
 
   const className = `flex gap-1 items-center text-md mt-1 mb-4`
   const countValue = `(${count}) reviews`
