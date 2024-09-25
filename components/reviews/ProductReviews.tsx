97c3c59db8b16682bfbf1a5f1fdb8cb05724d60a
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-09-25 09:25:21 +0800

    Create ReviewCard Component

--- components/reviews/ProductReviews.tsx
+++ components/reviews/ProductReviews.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { fetchProductReviews } from '@/utils/actions'
+import ReviewCard from './ReviewCard'
+import SectionTitle from '../global/SectionTitle'
 
-export default function ProductReviews() {
+async function ProductReviews({ productId }: { productId: string }) {
+  const reviews = await fetchProductReviews(productId)
+
   return (
-    <div>
-      
+    <div className='mt-16'>
+      <SectionTitle text='product reviews' />
+
+      <div className='grid md:grid-cols-2 gap-8 my-8'>
+        {reviews.map((review) => {
+          const { comment, rating, authorImageUrl, authorName } = review
+          const reviewInfo = {
+            comment,
+            rating,
+            image: authorImageUrl,
+            name: authorName,
+          }
+          return (
+            <ReviewCard
+              key={review.id}
+              reviewInfo={reviewInfo}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
+export default ProductReviews
--- utils/actions.ts
+++ utils/actions.ts
@@ -271,7 +271,7 @@ export const createReviewAction = async (
         clerkId: user.id,
       },
     })
-    
+
     revalidatePath(`/products/${validatedFields.productId}`)
     return { message: 'Review submitted successfully' }
   } catch (error) {
@@ -279,7 +279,17 @@ export const createReviewAction = async (
   }
 }
 
-export const fetchProductReviews = async () => {}
+export const fetchProductReviews = async (productId: string) => {
+  const reviews = await prisma.review.findMany({
+    where: {
+      productId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+  return reviews
+}
 export const fetchProductReviewsByUser = async () => {}
 export const deleteReviewAction = async () => {}
 export const findExistingReview = async () => {}
