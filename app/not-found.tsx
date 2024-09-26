c839c428ae7d64a71fbadee1b9b0240cd34ed53c
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-09-26 12:23:10 +0800

    Last development commit

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,24 @@
 import Link from 'next/link'
 import SectionTitle from '@/components/global/SectionTitle'
 import { Button } from '@/components/ui/button'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 export default function NotFound() {
   return (
     <div className='flex flex-col gap-10'>
-      <SectionTitle text='No such page' />
-      <p className='mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground'>
-        Could not find requested resource
-      </p>
+      <SectionTitle text='No such page or content' />
+      <Alert
+        variant='destructive'
+        className='text-2xl'
+      >
+        <AlertTitle>Error!</AlertTitle>
+        <AlertDescription className='text-xl'>
+          Could not find requested resource
+        </AlertDescription>
+      </Alert>
       <Button
         size='lg'
         variant='secondary'
+        className='self-center'
       >
         <Link href='/'>Return Home</Link>
       </Button>
